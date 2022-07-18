8ef37962f2429f3a7afa0211be13dc433f3882b5
Luke Brandon <lukebrandon68@gmail.com>
2022-07-18 09:20:48 -0500

    aliases

--- config.fish
+++ config.fish
@@ -1,3 +1,17 @@
+set -g -x fish_greeting ''
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Set vim mode
+set --export EDITOR "vim"
+
+# Aliases
+alias gcd="git checkout dev"
+alias gcs="git checkout staging"
+alias desk="bash ~/.screenlayout/desk.sh"
+alias laptop="bash ~/.screenlayout/laptop.sh"
+alias scrot="scrot -s ~/Pictures/scrot/"
+
+# Theme eclm
