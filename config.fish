3501bebc91a41a9643a34590ae5fb82134287c9a
Luke Brandon <lukebrandon68@gmail.com>
2022-11-29 09:35:52 -0600

    some aliases

--- config.fish
+++ config.fish
@@ -9,9 +9,14 @@ set --export EDITOR "vim"
 
 # Aliases
 alias gcd="git checkout dev"
+alias gcm="git checkout main"
 alias gcs="git checkout staging"
 alias desk="bash ~/.screenlayout/desk.sh"
 alias laptop="bash ~/.screenlayout/laptop.sh"
 alias scrot="scrot -s ~/Pictures/scrot/"
+alias ds="cd ~/source/acretrader-ds/"
+alias ui="cd ~/source/acremaps-ui/"
+alias yd="yarn dev"
+alias scrot="scrot -s ~/Pictures/Screenshots/%b%d::%H%M%S.png"
 
 # Theme eclm
